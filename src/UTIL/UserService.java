821d003b779bd52f2a5a8021438ea03f54039f24
myj4513 <yejungod@gmail.com>
2021-07-04 03:56:36 +0900

    update data structure/ refactoring

---
+++ src/UTIL/UserService.java
@@ -0,0 +1,35 @@
+package UTIL;
+
+import DTO.User;
+import DTO.Product;
+import java.util.*;
+
+public class UserService {
+    private static User currentUser = null;
+
+    public static void login(User user){
+        currentUser = user;
+    }
+
+    public static boolean isCurrentUserLoggedIn(){
+        return currentUser != null;
+    }
+
+    public static User getCurrentUser(){
+        return currentUser;
+    }
+
+    public static void addFundingHistory(Product product){
+        currentUser.getFundingHistory().add(product);
+    }
+
+    public static void showFundingHistory(){
+        Iterator it = currentUser.getFundingHistory().iterator();
+        int count = 0;
+        while(it.hasNext()){
+            Product product = (Product)it.next();
+            System.out.println(++count+"."+product.getName());
+        }
+        System.out.println();
+    }
+}
